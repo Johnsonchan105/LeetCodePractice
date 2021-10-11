c4ea9e3dbf1d4ae41e50cd1232dfaa81bb3fcfd3
Johnsonchan105 <johnsonchan105@gmail.com>
2021-10-10 21:43:28 -0700

    updated Solutions.java but still not right, updated test.java just for testing conditions

--- test.java
+++ test.java
@@ -1,9 +1,17 @@
 
 public class test {
-    public static void main(String[] args){
-        char[][] w = new char[3][4];
-        w = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]];
-        
+    public static void main(String[] args) {
+        /*
+         * char[][] w = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D',
+         * 'E', 'E' } }; String word = "ABCCED"; { { 'A', 'B', 'C', 'E' }, { 'S', 'F',
+         * 'C', 'S' }, { 'A', 'D', 'E', 'E' } } ABCB [["a","b"],["c","d"]]
+         * 
+         * 
+         */
+        char[][] w = { { 'a', 'b' }, { 'c', 'd' } };
+        String word = "abcd";
         Solution s = new Solution();
+        boolean a = s.exist(w, word);
+        System.out.println(a);
     }
 }
