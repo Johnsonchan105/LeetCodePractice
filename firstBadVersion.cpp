965b38e2a261becf463d0527ed4c354d05506576
Johnson Chan <johnsonchan105@gmail.com>
2022-01-18 20:29:39 -0800

    problem 278: better first bad version

--- firstBadVersion.cpp
+++ firstBadVersion.cpp
@@ -1,14 +1,28 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-class Solution {
+class Solution
+{
 public:
-    int firstBadVersion(int n) {
+    int firstBadVersion(int n)
+    {
+        /*
         for(int i = n; i >= 0; i--){
             if(!isBadVersion(i)){
                 return i + 1;
             }
         }
         return -1;
+        */
+        int lower = 1, upper = n, mid;
+        while (lower < upper)
+        {
+            mid = lower + (upper - lower) / 2;
+            if (!isBadVersion(mid))
+                lower = mid + 1;
+            else
+                upper = mid;
+        }
+        return lower;
     }
 };
