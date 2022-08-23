1cab68e62dc5910193355989f14df7718522e933
Johnson Chan <johnsonchan@Johnsons-MacBook-Pro.local>
2022-08-22 17:35:12 -0700

    Problem 234: Palindrome linked list

---
+++ problem234.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> store;
+        while(head != nullptr){
+            store.push_back(head->val);
+            head = head->next;
+        }
+        for(int i = 0; i < store.size()/2; i++){
+            if(store[i] != store[store.size()-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
